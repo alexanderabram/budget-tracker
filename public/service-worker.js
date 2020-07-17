a29e90452996c46d04ce56a1556c274366aee82b
AMA <alexanderabram6440@gmail.com>
2020-07-17 15:01:44 -0500

    updated files to cache

--- public/service-worker.js
+++ public/service-worker.js
@@ -1,7 +1,11 @@
 const FILES_TO_CACHE = [
-    "/", "/index.html", "index.js", "/db.js", "/style.css"];
+    "/", 
+    "/index.html", 
+    "index.js", 
+    "/db.js", 
+    "/style.css"];
 
-const CACHE_NAME = "static-cache-v2";
+const CACHE_NAME = "static-cache-v1";
 const DATA_CACHE_NAME = "data-cache-v1";
 
 // install
